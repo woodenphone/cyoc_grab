6894eb5d2280f3b935b8e153a556fb256be102d6
James <j1459.j1459@gmail.com>
2016-06-19 15:41:06 +0800

    work on validation code

--- validate_warc.py
+++ validate_warc.py
@@ -19,8 +19,18 @@ def validate_record(record):
     """Validate a page from a CYOC.net grab warc file"""
     assert(record.type == 'response')
 
-    if ('cyoc.net/modules.php' in record.url):
+    if ('cyoc.net/modules.php?op=modload&name=Stories&file=article' in record.url):
         # Validate a basic story page
+        # http://www.cyoc.net/modules.php?op=modload&name=Stories&file=article&sid=1969&mode=nested&order=0&thold=-1
+        html = record.payload.read()
+        # Ensure user was logged in
+        assert('&op=logout' in html)
+        assert('You must be logged in to view this site' not in html)
+        assert('Please login or Register Via the login box on the lef' not in html)
+
+    elif ( 'cyoc.net/modules.php?op=modload&name=Image_Stories' in record.url ):
+        # Validate a image story page
+        # http://www.cyoc.net/modules.php?op=modload&name=Image_Stories&file=view_story&story_id=1341
         html = record.payload.read()
         # Ensure user was logged in
         assert('&op=logout' in html)
@@ -29,6 +39,7 @@ def validate_record(record):
 
     elif ( ('cyoc.net/interactives/' in record.url) and (record.url.endswith('outline.html')) ):
         # Validate a CYOA outline page
+        # http://www.cyoc.net/interactives/story_50/outline.html
         html = record.payload.read()
         # Ensure user was logged in
         assert('&op=logout' in html)
@@ -36,11 +47,17 @@ def validate_record(record):
 
     elif ('cyoc.net/interactives/chapter' in record.url):
         # Validate a CYOA chapter page
+        # http://www.cyoc.net/interactives/chapter_105806.html
         html = record.payload.read()
         # Ensure user was logged in
         assert('&op=logout' in html)
         assert('You are not logged in. Log in' not in html)
 
+    else:
+        # Not a targeted page
+        return True
+
+    print('Validated %s' % (record.url))
     return True# If all tests passed.
 
 
