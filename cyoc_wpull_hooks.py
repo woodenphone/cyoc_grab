3bac883bb0c7b79b6663e1d1d759875621a4dc6d
James <j1459.j1459@gmail.com>
2016-06-20 23:41:26 +0800

    this does not work yet

--- cyoc_wpull_hooks.py
+++ cyoc_wpull_hooks.py
@@ -1,89 +1,49 @@
 print('cyoc_wpull_hooks.py first line')
-# https://github.com/ArchiveTeam/example-wpull-seesaw-project/blob/master/examplecity.py
-import sys
 
-wpull_hook = globals().get('wpull_hook')  # silence code checkers
+import datetime
+import re
 
-counter = 0
+from wpull.application.hook import Actions
+from wpull.application.plugin import WpullPlugin, PluginFunctions, hook
+from wpull.protocol.abstract.request import BaseResponse
+from wpull.pipeline.session import ItemSession
 
+print('cyoc_wpull_hooks.py after imports')
 
-print('cyoc_wpull_hooks.py before functions')
+class MyExamplePlugin(WpullPlugin):
+    def activate(self):
+        super().activate()
+        print('Hello world!')
 
-def engine_run():
-    print('    Hello world!')
-    sys.stdout.flush()
+    def deactivate(self):
+        super().deactivate()
+        print('Goodbye world!')
 
+    @hook(PluginFunctions.accept_url)
+    def my_accept_func(self, item_session: ItemSession, verdict: bool, reasons: dict) -> bool:
+        print('cyoc_wpull_hooks.py my_accept_func() start')
+        return 'dog' not in item_session.request.url
 
-def resolve_dns(host):
-    print('cyoc_wpull_hooks.py resolve_dns()')
-    pass
+    @event(PluginFunctions.get_urls)
+    def my_get_urls(self, item_session: ItemSession):
+        print('cyoc_wpull_hooks.py my_get_urls() start')
+        if item_session.request.url_info.path != '/':
+            return
 
+        matches = re.finditer(
+            r'<div id="profile-(\w+)"', item_session.response.body.content
+        )
+        for match in matches:
+            url = 'http://example.com/profile.php?username={}'.format(
+                match.group(1)
+            )
+            item_session.add_child_url(url)
 
-def accept_url(url_info, record_info, verdict, reasons):
-    print('cyoc_wpull_hooks.py accept_url()')
-    return verdict
+    @hook(PluginFunctions.handle_response)
+    def my_handle_response(item_session: ItemSession):
+        print('cyoc_wpull_hooks.py my_handle_response() start')
+        if item_session.response.response_code == 429:
+            return Actions.STOP
 
-
-def queued_url(url_info):
-    global counter
-    print('    queued_url', url_info['url'])
-    sys.stdout.flush()
-    counter += 1
-
-
-def dequeued_url(url_info, record_info):
-    global counter
-    print('    dequeued_url', url_info['url'])
-    sys.stdout.flush()
-    counter -= 1
-
-
-def handle_pre_response(url_info, record_info, http_info):
-    print('cyoc_wpull_hooks.py handle_pre_response()')
-    return wpull_hook.actions.NORMAL
-
-
-def handle_response(url_info, record_info, http_info):
-    print('cyoc_wpull_hooks.py handle_response()')
-    return wpull_hook.actions.NORMAL
-
-
-def handle_error(url_info, record_info, error_info):
-    print('cyoc_wpull_hooks.py handle_error()')
-    return wpull_hook.actions.NORMAL
-
-
-def get_urls(filename, url_info, document_info):
-    print('cyoc_wpull_hooks.py get_urls()')
-    return None
-
-
-def wait_time(seconds):
-    print('cyoc_wpull_hooks.py wait_time()')
-    return seconds
-
-
-def finish_statistics(start_time, end_time, num_urls, bytes_downloaded):
-    pass
-
-
-def exit_status(exit_code):
-    print('cyoc_wpull_hooks.py exit_status()')
-    return exit_code
-
-print('cyoc_wpull_hooks.py after functions')
-
-wpull_hook.callbacks.engine_run = engine_run
-wpull_hook.callbacks.resolve_dns = resolve_dns
-wpull_hook.callbacks.accept_url = accept_url
-wpull_hook.callbacks.queued_url = queued_url
-wpull_hook.callbacks.dequeued_url = dequeued_url
-wpull_hook.callbacks.handle_pre_response = handle_pre_response
-wpull_hook.callbacks.handle_response = handle_response
-wpull_hook.callbacks.handle_error = handle_error
-wpull_hook.callbacks.get_urls = get_urls
-wpull_hook.callbacks.wait_time = wait_time
-wpull_hook.callbacks.finish_statistics = finish_statistics
-wpull_hook.callbacks.exit_status = exit_status
 
 print('cyoc_wpull_hooks.py last line')
