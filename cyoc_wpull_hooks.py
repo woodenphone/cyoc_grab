9f83d469c28207b09f4f22f2e39f02fae2ea795e
James <j1459.j1459@gmail.com>
2016-06-20 23:52:34 +0800

    PROGRESS

--- cyoc_wpull_hooks.py
+++ cyoc_wpull_hooks.py
@@ -4,46 +4,52 @@ import datetime
 import re
 
 from wpull.application.hook import Actions
-from wpull.application.plugin import WpullPlugin, PluginFunctions, hook
+from wpull.application.plugin import WpullPlugin, PluginFunctions, hook, event
+
 from wpull.protocol.abstract.request import BaseResponse
 from wpull.pipeline.session import ItemSession
 
 print('cyoc_wpull_hooks.py after imports')
 
-class MyExamplePlugin(WpullPlugin):
-    def activate(self):
-        super().activate()
-        print('Hello world!')
+try:
 
-    def deactivate(self):
-        super().deactivate()
-        print('Goodbye world!')
+    class MyExamplePlugin(WpullPlugin):
+        print('cyoc_wpull_hooks.py inside class')
+        def activate(self):
+            super().activate()
+            print('Hello world!')
 
-    @hook(PluginFunctions.accept_url)
-    def my_accept_func(self, item_session: ItemSession, verdict: bool, reasons: dict) -> bool:
-        print('cyoc_wpull_hooks.py my_accept_func() start')
-        return 'dog' not in item_session.request.url
+        def deactivate(self):
+            super().deactivate()
+            print('Goodbye world!')
 
-    @event(PluginFunctions.get_urls)
-    def my_get_urls(self, item_session: ItemSession):
-        print('cyoc_wpull_hooks.py my_get_urls() start')
-        if item_session.request.url_info.path != '/':
-            return
+        @hook(PluginFunctions.accept_url)
+        def my_accept_func(self, item_session, verdict, reasons):
+            print('cyoc_wpull_hooks.py my_accept_func() start')
+            return 'dog' not in item_session.request.url
 
-        matches = re.finditer(
-            r'<div id="profile-(\w+)"', item_session.response.body.content
-        )
-        for match in matches:
-            url = 'http://example.com/profile.php?username={}'.format(
-                match.group(1)
+        @event(PluginFunctions.get_urls)
+        def my_get_urls(self, item_session):
+            print('cyoc_wpull_hooks.py my_get_urls() start')
+            if item_session.request.url_info.path != '/':
+                return
+
+            matches = re.finditer(
+                r'<div id="profile-(\w+)"', item_session.response.body.content
             )
-            item_session.add_child_url(url)
+            for match in matches:
+                url = 'http://example.com/profile.php?username={}'.format(
+                    match.group(1)
+                )
+                item_session.add_child_url(url)
 
-    @hook(PluginFunctions.handle_response)
-    def my_handle_response(item_session: ItemSession):
-        print('cyoc_wpull_hooks.py my_handle_response() start')
-        if item_session.response.response_code == 429:
-            return Actions.STOP
+        @hook(PluginFunctions.handle_response)
+        def my_handle_response(self, item_session):
+            print('cyoc_wpull_hooks.py my_handle_response() start')
+            if item_session.response.response_code == 429:
+                return Actions.STOP
 
+except Exception as err:
+    print(repr(err))
 
 print('cyoc_wpull_hooks.py last line')
