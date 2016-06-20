91aee5c75e3675e26eb1b23bbdb58fc27dda68cb
James <j1459.j1459@gmail.com>
2016-06-21 01:26:22 +0800

    Seems okay now

--- cyoc_wpull_hooks.py
+++ cyoc_wpull_hooks.py
@@ -1,26 +1,23 @@
 # encoding=utf-8
 # This file is python 3 code because that's what Wpull wants
-print('cyoc_wpull_hooks.py first line')
-
 import datetime
 import re
 
 from wpull.application.hook import Actions
 from wpull.application.plugin import WpullPlugin, PluginFunctions, hook, event
-
 from wpull.protocol.abstract.request import BaseResponse
 from wpull.pipeline.session import ItemSession
 
-print('cyoc_wpull_hooks.py after imports')
 
-class MyExamplePlugin(WpullPlugin):
-
+class CYOCPlugin(WpullPlugin):
+    """Kill the Wpull run if we aren't logged in at some point"""
     @hook(PluginFunctions.handle_response)
     def my_handle_response(self, item_session):
         print('cyoc_wpull_hooks.py my_handle_response() start')
         if item_session.response.response_code == 429:
             return Actions.STOP
         page_data = str( item_session.response.body.content() )
+        page_data = page_data.lower()
         #print(''.format())
 
         # Kill run if not logged in
@@ -30,11 +27,9 @@ class MyExamplePlugin(WpullPlugin):
             'You are not logged in. Log in',
         ]
         for exclude_string in exclude_strings:
+            exclude_string = exclude_string.lower()
             if (exclude_string in page_data):
                 print('cyoc_wpull_hooks.py exclude_string matched: {0}'.format(exclude_string))
                 return Actions.STOP
 
         return Actions.NORMAL# If we don't have any input to make
-
-
-print('cyoc_wpull_hooks.py last line')
